2c0d9407b2e786d1c7c4499403f3fa364397f4ad
gowriekaran <g.karan.sinna@gmail.com>
2023-10-05 17:16:54 -0400

    v2.0.3
    Added Position class

---
+++ Prototype 1/position.py
@@ -0,0 +1,15 @@
+class Position:
+    """
+    Private class for representing submission information.
+
+    Attributes:
+        __Name (str): The name of the position.
+        __Description (str): The description of the position.
+        __Status (str): The status of the position.
+    """
+    
+    """ class-level attributes with double underscores to enforce encapsulation and make attributes private """
+
+    __Name = None
+    __Description = None
+    __Status = None
