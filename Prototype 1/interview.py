8ef5f37927ba026c187cbff3054c417ff0f85b87
gowriekaran <g.karan.sinna@gmail.com>
2023-10-05 17:24:13 -0400

    v2.0.5
    Added Interview class

---
+++ Prototype 1/interview.py
@@ -0,0 +1,19 @@
+class Interview:
+    """
+    Private class for representing student information.
+
+    Attributes:
+        __date (date): The interview date.
+        __time (time): The interview time.
+        __location (str): The interview location.
+        __feedback (str): The optional feedback a president can add to the interview record.
+        __status (str): The status of the interview..
+    """
+    
+    """ class-level attributes with double underscores to enforce encapsulation and make attributes private """
+
+    __date = None
+    __time = None
+    __location = None
+    __feedback = None
+    __status = None
