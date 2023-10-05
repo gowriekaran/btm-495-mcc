9baa40906e2885e2308169eb4e8f82e84ce622fc
gowriekaran <g.karan.sinna@gmail.com>
2023-10-05 17:25:56 -0400

    v2.0.6
    Added Offer class

---
+++ Prototype 1/offer.py
@@ -0,0 +1,13 @@
+class Offer:
+    """
+    Private class for representing student information.
+
+    Attributes:
+        __details (str): The details of the offer.
+        __positionID (int): The unique identifier of the position being offered.
+    """
+    
+    """ class-level attributes with double underscores to enforce encapsulation and make attributes private """
+
+    __details = None
+    __positionID = None
