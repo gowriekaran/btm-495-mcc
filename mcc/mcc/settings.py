51797a933332581115025c9f9398a1fc7d2bd3b1
gowriekaran <g.karan.sinna@gmail.com>
2023-11-03 20:08:03 -0400

    v3.0.1
    Swapped to TailwindCSS app from default app. Removed default app

--- mcc/mcc/settings.py
+++ mcc/mcc/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'website',
     'django_browser_reload',
     'tailwind',
     'theme'
--- mcc/theme/apps.py
+++ mcc/theme/apps.py
@@ -3,3 +3,7 @@ from django.apps import AppConfig
 
 class ThemeConfig(AppConfig):
     name = 'theme'
+
+# class WebsiteConfig(AppConfig):
+#     default_auto_field = 'django.db.models.BigAutoField'
+#     name = 'website'
