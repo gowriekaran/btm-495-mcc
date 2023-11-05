50a7f543e888926ee0c10a6771068489c6ee9090
Gowriekaran Sinnadurai <gowriekaran@users.noreply.github.com>
2023-11-05 00:23:51 -0400

    v3.3.3
    Made it so positions can only be Vacant, Open, Shortlist, Interview, Offered or Filled

--- mcc/website/models.py
+++ mcc/website/models.py
@@ -1,10 +1,26 @@
 from django.db import models
 
 class Position(models.Model):
+    VACANT = 'Vacant'
+    OPEN = 'Open'
+    SHORTLIST = 'Shortlist'
+    INTERVIEW = 'Interview'
+    OFFERED = 'Offered'
+    FILLED = 'Filled'
+
+    STATUS_CHOICES = [
+        (VACANT, 'Vacant'),
+        (OPEN, 'Open'),
+        (SHORTLIST, 'Shortlist'),
+        (INTERVIEW, 'Interview'),
+        (OFFERED, 'Offered'),
+        (FILLED, 'Filled'),
+    ]
+
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255)
     description = models.CharField(max_length=255)
-    status = models.CharField(max_length=255)
+    status = models.CharField(max_length=255, choices=STATUS_CHOICES, default=VACANT)
 
     def __str__(self):
         return f"Position: {self.id} - {self.name} - {self.description} - {self.status}"
